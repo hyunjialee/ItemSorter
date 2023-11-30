1d3bce2a93cf06691c660d6c2893dd6843045d19
hyunjialee <hyjialee@gmail.com>
2023-11-30 09:43:16 -0500

    finished

--- src/main/java/rocks/zipcode/io/comparators/NameComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/NameComparator.java
@@ -13,7 +13,17 @@ public class NameComparator implements Comparator<Item> {
         // if statements
         // compares each the letters
 
+        String x = item1.getName();
+        String y = item2.getName();
 
-        return item1.getName().compareToIgnoreCase(item2.getName());
+        for (int i = 0; i < Math.min(x.length(), y.length()); i++) {
+            int name1 = x.charAt(i);
+            int name2 = y.charAt(i);
+
+            if (name1 != name2) {
+                return Integer.compare(name1, name2);
+            }
+        }
+        return 0;
     }
 }
